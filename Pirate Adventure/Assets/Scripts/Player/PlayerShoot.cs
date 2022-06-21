be800ad61b847a9310502ace7be38e4ac793c8fa
thewebrage <ein.nuff@gmail.com>
2022-06-20 21:23:59 -0600

    Bullet not quite working

--- Pirate Adventure/Assets/Scripts/Player/PlayerShoot.cs
+++ Pirate Adventure/Assets/Scripts/Player/PlayerShoot.cs
@@ -7,15 +7,28 @@ using UnityEngine;
 /// </summary>
 public class PlayerShoot : MonoBehaviour
 {
+    // Bullet preFab that is being fired
+    public GameObject bullet;
+    public float bulletSpeed = 35;
+    private Rigidbody2D bulletRb;
+
+    private Rigidbody2D rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        bulletRb = bullet.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            bulletRb.transform.position = rb.transform.position;
+            Instantiate(bullet);
+            bulletRb.velocity = new Vector2(rb.velocity.x + bulletSpeed, rb.velocity.y + bulletSpeed);
+        }
     }
 }
